fb59d102d3fa134b8d69bff13998bb642e2e177d
Felix-F <felixferreras2002@gmail.com>
2023-12-22 12:21:03 -0500

    try this

--- cat-fact-app/routes/index.js
+++ cat-fact-app/routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+const fetch = require('node-fetch');
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'The Cat App !' });
 });
 
 module.exports = router;
